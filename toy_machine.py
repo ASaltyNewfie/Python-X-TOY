b6956423daa0cf2480cf6291197419440576d35c
ASaltyNewfie <70762024+ASaltyNewfie@users.noreply.github.com>
2022-07-04 20:12:27 -0230

    Improved names and readability
    Updated variable and function names to be more consistent with PEP-8.

--- toy_machine.py
+++ toy_machine.py
@@ -1,18 +1,20 @@
 import sys
 import re
 
-PC = 16
-R = {}
-M = {}
+program_counter = 16
+registers = {}
+memory = {}
 debug_mode = False
+
 
 def debug(out):
     if debug_mode: print(out)
 
-def execute():
-    global PC
 
-    instruction = M[hex(PC)]
+def execute():
+    global program_counter
+
+    instruction = load_memory(convert_to_hex_string(program_counter))
 
     opcode = instruction[0]
     d = instruction[1]
@@ -20,14 +22,14 @@ def execute():
     t = instruction[3]
     addr = instruction[2:4]
 
-    a = int(R[s], 16)
-    b = int(R[t], 16)
+    a = int(registers[s], 16)
+    b = int(registers[t], 16)
 
     if opcode == '1':
-        debug(f'Adding register {s} ({R[s]}) to register {t} ({R[t]}) and storing the result in register {d}')
+        debug(f'Adding register {s} ({registers[s]}) to register {t} ({registers[t]}) and storing the result in register {d}')
         math_op(d, a + b)
     if opcode == '2':
-        debug(f'Subtracting register {t} ({R[t]}) from register {s} ({R[s]}) and storing the result in register {d}')
+        debug(f'Subtracting register {t} ({registers[t]}) from register {s} ({registers[s]}) and storing the result in register {d}')
         math_op(d, a - b)
     if opcode == '3':
         debug(f'Performing binary AND on registers {s} and {t} and storing the result in register {d}')
@@ -36,10 +38,10 @@ def execute():
         debug(f'Performing binary OR on registers {s} and {t} and storing the result in register {d}')
         math_op(d, a ^ b)
     if opcode == '5':
-        debug(f'Performing binary LEFT SHIFT on registers {s} by {t} and storing the result in register {d}')
+        debug(f'Performing binary left shift on registers {s} by {t} and storing the result in register {d}')
         math_op(d, a << b)
     if opcode == '6':
-        debug(f'Performing binary RIGHT SHIFT on registers {s} by {t} and storing the result in register {d}')
+        debug(f'Performing binary right shift on registers {s} by {t} and storing the result in register {d}')
         math_op(d, a >> b)
 
     if opcode == '7':
@@ -48,56 +50,59 @@ def execute():
     if opcode == '8':
         if addr == 'FF':
             debug(f'Storing input in memory {addr}')
-            M[addr] = input(': ')
-        debug(f'Storing memory {addr} ({M[addr]}) in register {d}')
-        store_register(d, M[addr])
+            memory[addr] = input(': ')
+        debug(f'Storing memory {addr} ({memory[addr]}) in register {d}')
+        store_register(d, memory[addr])
     if opcode == '9':
         if addr == 'FF':
-            debug(f'Storing register {d} ({R[d]}) in memory {addr} and printing {R[d]}')
+            debug(f'Storing register {d} ({registers[d]}) in memory {addr} and printing {registers[d]}')
         else:
-            debug(f'Storing register {d} ({R[d]}) in memory {addr}')
-        store_memory(addr, R[d])
-    if opcode == 'A':
-        debug(f'Storing register {d} ({R[d]}) in memory {R[t]} and printing {R[d]}')
-        store_register(d, load_memory(R[t]))
+            debug(f'Storing register {d} ({registers[d]}) in memory {addr}')
+        store_memory(addr, registers[d])
+    if opcode == 'A': # Store
+        debug(f'Storing register {d} ({registers[d]}) in memory {registers[t]} and printing {registers[d]}')
+        store_register(d, load_memory(registers[t]))
     if opcode == 'B':
-        if R[t] == 'FF':
-            debug(f'Storing register {d} ({R[d]}) in memory {R[t]} and printing {R[d]}')
+        if registers[t] == 'FF':
+            debug(f'Storing register {d} ({registers[d]}) in memory {registers[t]} and printing {registers[d]}')
         else:
-            debug(f'Storing register {d} ({R[d]}) in memory {R[t]}')
-        store_memory(R[t], R[d])
+            debug(f'Storing register {d} ({registers[d]}) in memory {registers[t]}')
+        store_memory(registers[t], registers[d])
 
     if opcode == '0':
         debug('Halting')
         return True
     if opcode == 'C':
-        if int(R[d], 16) == 0:
-            debug(f'Checked if register {d} ({R[d]}) was equal to zero - it was, so set the PC to {int(addr, 16)}')
-            PC = int(addr, 16) - 1
+        if int(registers[d], 16) == 0:
+            debug(f'Checked if register {d} ({registers[d]}) was equal to zero - it was, so set the program counter to {int(addr, 16)}')
+            program_counter = int(addr, 16) - 1
         else:
-            debug(f'Checked if register {d} ({R[d]}) was equal to zero - it was not')
+            debug(f'Checked if register {d} ({registers[d]}) was equal to zero - it was not')
     if opcode == 'D':
-        if int(R[d], 16) > 0:
-            debug(f'Checked if register {d} ({R[d]}) was greater than zero - it was, so set the PC to {int(addr, 16)}')
-            PC = int(addr, 16) - 1
+        if int(registers[d], 16) > 0:
+            debug(f'Checked if register {d} ({registers[d]}) was greater than zero - it was, so set the program counter to {int(addr, 16)}')
+            program_counter = int(addr, 16) - 1
         else:
-            debug(f'Checked if register {d} ({R[d]}) was greater than zero - it was not')
+            debug(f'Checked if register {d} ({registers[d]}) was greater than zero - it was not')
     if opcode == 'E':
-        debug(f'Setting the PC to register {d} ({int(R[d], 16) + 1})')
-        PC = int(R[d], 16)
+        debug(f'Setting the program counter to register {d} ({int(registers[d], 16) + 1})')
+        program_counter = int(registers[d], 16)
     if opcode == 'F':
-        print(f'Storing the PC ({hex(PC)}) in register {d} and setting the PC to {int(addr, 16)}')
-        store_register(d, hex(PC))
-        PC = int(addr, 16) - 1
+        print(f'Storing the program counter ({convert_to_hex_string(program_counter)}) in register {d} and setting the program counter to {int(addr, 16)}')
+        store_register(d, convert_to_hex_string(program_counter))
+        program_counter = int(addr, 16) - 1
 
-    PC += 1
+    program_counter += 1
     return False
 
-def hex(i, digits = 2):
+
+def convert_to_hex_string(i, digits = 2):
     return format(int(i), '0' + str(digits) + 'X')
 
-def load_memory(addr):
-    return M[addr[-2:]]
+
+def load_memory(location):
+    return memory[location[-2:]]
+
 
 def main():
     global debug_mode
@@ -112,35 +117,39 @@ def main():
     with open(file_name, 'r') as f:
         for line in f:
             if re.match(r'^[0-9A-F]{2}: [0-9A-F]{4}', line):
-                M[line[0:2]] = line[4:8]
+                memory[line[0:2]] = line[4:8]
 
     while True:
         halt = execute()
-        if halt or PC > 255: break
+        if halt or program_counter > 255: break
 
-def math_op(d, value):
+
+def math_op(destination, value):
     if value < -32768 or 32767 < value:
-        raise Exception(f'Error at {hex(PC)}: Operation outside the range of -32768 and 32767 ({str(value)})')
-    store_register(d, hex(value, 4))
+        raise Exception(f'Error at {convert_to_hex_string(program_counter)}: Operation outside the range of -32768 and 32767 ({str(value)})')
+    store_register(destination, convert_to_hex_string(value, 4))
 
-def store_memory(addr, value):
-    addr = str(addr).zfill(2)[-2:]
+
+def store_memory(address, value):
+    addr = str(address).zfill(2)[-2:]
     value = str(value).zfill(4)
     if addr == 'FF':
         print('> ' + value + ',', int(value, 16))
-    M[addr] = value
+    memory[addr] = value
 
-def store_register(addr, value):
-    addr = str(addr)[-1:]
+
+def store_register(address, value):
+    addr = str(address)[-1:]
     value = str(value).zfill(4)
     if addr == '00':
-        raise Exception(f'Error at {hex(PC)}: Register 00 is reserved')
-    R[addr] = value
+        raise Exception(f'Error at {convert_to_hex_string(program_counter)}: Register 00 is reserved')
+    registers[addr] = value
+
 
 if __name__ == '__main__':
     for i in range(16):
-        R[hex(i, 1)] = '0000'
+        registers[convert_to_hex_string(i, 1)] = '0000'
     for i in range(256):
-        M[hex(i)] = '0000'
+        memory[convert_to_hex_string(i)] = '0000'
 
     main()
